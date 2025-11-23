29d625250a79e4cf57d9430c5824520cc6a93f37
Andreas Vural <andreasvural@AVural-x-MacBook-Air.local>
2025-11-23 22:39:38 +0700

    Add Capacitor configuration for Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.mumsscale',
+  appName: 'MUMS Scale',
+  webDir: 'dist'
+};
+
+export default config;
